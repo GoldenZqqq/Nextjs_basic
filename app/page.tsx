5b5a19a6704bbe600df18df4770ae072857c90a4
GoldenZqqq <1361001127@qq.com>
2024-12-18 15:31:05 +0800

    refactor: simplify layout and remove unused pages; update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image"
 
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to Next.js</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}></Image>
+    <main className="bg-red-500 h-screen">
     </main>
   )
 }
