d7ddc6b912a221ba5e73f6e1bd19c8767e011967
GoldenZqqq <1361001127@qq.com>
2024-12-26 10:36:10 +0800

    feat: enhance article API with pagination and search functionality, update article content in db.json, and improve response messages in Chinese

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -16,12 +16,31 @@ export async function DELETE(request: Request, { params }: IParams) {
 
   return NextResponse.json({
     code: 0,
-    message: "success"
+    message: "删除成功"
   })
 }
 
 // PATCH => /api/articles/:id
-export async function PATCH(request: Request) {}
+export async function PATCH(request: Request, { params }: IParams) {
+  const data = await request.json()
+  let idx = -1
+  await db.update(({ posts }) => {
+    idx = posts.findIndex(post => post.id === params.id)
+    posts[idx] = { ...posts[idx], ...data }
+  })
+  return NextResponse.json({
+    code: 0,
+    message: "修改成功",
+    data: db.data.posts[idx]
+  })
+}
 
 // GET => /api/articles/:id
-export async function GET(request: Request) {}
+export async function GET(request: Request, { params }: IParams) {
+  const post = await db.data.posts.find(post => post.id === params.id)
+  return NextResponse.json({
+    code: 0,
+    message: "查找成功",
+    data: post
+  })
+}
--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,8 +1,40 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import db from "@/db"
 
 // GET => /api/articles
-export async function GET(request: Request) {}
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl
+  const pageNum = Number(searchParams.get("pageNum")) || 1
+  const pageSize = Number(searchParams.get("pageSize")) || 1
+  const query = searchParams.get("query") || ""
+
+  const data = db.data.posts
+
+  let filteredData = query
+    ? data.filter(item => {
+        const { id, ...rest } = item
+        return Object.values(rest).some(value =>
+          value.toString().toLowerCase().includes(query.toLowerCase())
+        )
+      })
+    : data
+
+  const total = filteredData.length
+
+  const startIndex = (pageNum - 1) * pageSize
+  const endIndex = Math.min(startIndex + pageSize, total)
+  filteredData =
+    startIndex >= total ? [] : filteredData.slice(startIndex, endIndex)
+
+  return NextResponse.json({
+    code: 0,
+    message: "获取成功",
+    data: {
+      total,
+      list: filteredData
+    }
+  })
+}
 
 // POST => /api/articles
 export async function POST(request: Request) {
