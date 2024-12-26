443260d7752bc32fa95df393c41447d7e7c007dc
GoldenZqqq <1361001127@qq.com>
2024-12-26 09:44:39 +0800

    feat: implement CRUD operations for articles with lowdb integration, add initial data structure in db.json, and enhance TypeScript typings in db.ts

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -1,5 +1,24 @@
+import { NextResponse } from "next/server"
+import db from "@/db"
+
+interface IParams {
+  params: {
+    id: string
+  }
+}
+
 // DELETE => /api/articles/:id
-export async function DELETE(request: Request) {}
+export async function DELETE(request: Request, { params }: IParams) {
+  await db.update(({ posts }) => {
+    const idx = posts.findIndex(post => post.id === params.id)
+    posts.splice(idx, 1)
+  })
+
+  return NextResponse.json({
+    code: 0,
+    message: "success"
+  })
+}
 
 // PATCH => /api/articles/:id
 export async function PATCH(request: Request) {}
--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server"
+import db from "@/db"
 
 // GET => /api/articles
-export async function GET(request: Request) {
-}
+export async function GET(request: Request) {}
 
 // POST => /api/articles
 export async function POST(request: Request) {
   const data = await request.json()
+  await db.update(({ posts }) =>
+    posts.unshift({
+      id: Math.random().toString(36).slice(2, 10),
+      ...data
+    })
+  )
   return NextResponse.json({
     code: 0,
     message: "success",
