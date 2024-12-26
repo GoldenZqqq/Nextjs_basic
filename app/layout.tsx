b155f328a85e1e636d849e81b6d2d1ad0036bcf8
GoldenZqqq <1361001127@qq.com>
2024-12-26 11:48:59 +0800

    feat: add NotFound page and time API endpoint, refactor RootLayout to remove modal prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,18 +11,15 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-  modal
-}: Readonly<{
+  children
+}: {
   children: React.ReactNode
-  modal: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
         <AntdRegistry>
           {children}
-          {modal}
         </AntdRegistry>
       </body>
     </html>
